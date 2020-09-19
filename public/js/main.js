f35f967cfb5b14f6750884ff591e7cdba5ae92f3
Masoud-Yari <masoudd.yarii@gmail.com>
2020-09-19 22:45:04 +0430

    fix overflow issue and reduce size of images

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,13 @@
 const message = document.querySelector('.message');
 
-if(message.innerHTML === ''){
-    message.style.right = '0';
-}else{
-    message.style.right = '20px';
-    setTimeout(() => {
-        message.style.right = '-100%'
-        message.innerHTML = '';
-    }, 5000);
+if(message){
+    if(message.innerHTML === ''){
+        message.style.right = '0';
+    }else{
+        message.style.right = '20px';
+        setTimeout(() => {
+            message.style.right = '-100%'
+            message.innerHTML = '';
+        }, 5000);
+    }
 }
